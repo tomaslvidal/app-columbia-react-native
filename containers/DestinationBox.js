10aaba5f9fb6f0461ca0bce99f6929b346d1ff8c
Vidal Tomás <vtcheqeeh@gmail.com>
2018-10-11 17:59:05 -0300

    upgrade destinationBox

--- containers/DestinationBox.js
+++ containers/DestinationBox.js
@@ -1,64 +1,47 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ImageBackground } from 'react-native';
 
-export default class DestinationBox extends Component {
-  
+import {View, Text, StyleSheet, ImageBackground} from 'react-native';
+
+export default class DestinationBox extends Component{
   constructor(props){
-      super(props);
+    super(props);
   }
 
-  componentDidMount(){
-    //this.updateDataSource(this.props.destinations)
-  }
-
-/*  componentWillReceiveProps(newProps) {
-    
-    if(newProps.destinations !== this.props.destinations) {
-
-    }
-    
-  }*/
-
-  render() {
-
-
-
-    return (
-        <View style={[styles.boxDefault, { backgroundColor: 'white', alignSelf: 'stretch' }]}>
-           <ImageBackground style={[ styles.imageBackground, {} ]} source={{ uri: this.props.destination.img }}>
-            <View style={[{flex: 1}]}>
-              <Text style={[ styles.text ]}>
-                {this.props.destination.name}
-              </Text>
-            </View>
-          </ImageBackground>
-        </View>
+  render(){
+    return(
+      <View style={[styles.boxDefault]}>
+        <ImageBackground style={[styles.imageBackground, {}]} source={{uri: this.props.destination.img}}>
+          <View style={[{flex: 1}]}>
+            <Text style={[styles.text]}>
+              {this.props.destination.name}
+            </Text>
+          </View>
+        </ImageBackground>
+      </View>
     );
-
   }
 }
 
-
 const styles = StyleSheet.create({
-    boxDefault: {
-      height: 200,
-      paddingBottom: 5
-    },
-    imageBackground:{
-      height: '100%',
-      width: '100%',
-      borderRadius: 4,
-      borderWidth: 0.5,
-      borderColor: '#d6d7da'
-    },
-    text: {
-      fontSize: 33,
-      paddingTop: 18,
-      paddingLeft: 20,
-      color: '#FFF', 
-      paddingBottom: 18,
-      textShadowColor: "#000", 
-      textShadowRadius: 7, 
-      textShadowOffset: { height: 2, width: -0 }
-    }
+  boxDefault:{
+    height: 175,
+    paddingBottom: 5
+  },
+  imageBackground:{
+    height: '100%',
+    width: '100%',
+    borderRadius: 4,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da'
+  },
+  text:{
+    fontSize: 33,
+    paddingTop: 18,
+    paddingLeft: 20,
+    color: '#FFF', 
+    paddingBottom: 18,
+    textShadowColor: "#000", 
+    textShadowRadius: 23, 
+    textShadowOffset: {height: 2, width: 1}
+  }
 });
