49ba666b9ebb66fe5c8f6f134680f3d15130310d
Vidal Tomás <vtcheqeeh@gmail.com>
2018-10-11 12:52:32 -0300

    add fileComponent

---
+++ components/FileComponent.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import { Text, View, StyleSheet, Image, TouchableOpacity, Linking } from 'react-native';
+
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+export default class FileComponent extends Component{
+  render(){
+    return(
+      <View style={(this.props.style!=undefined && this.props.style!="") ? [styles.body, this.props.style] : styles.body}>
+        <View>
+          <Text style={styles.name}>{this.props.name}</Text>
+        </View>
+        
+        <TouchableOpacity onPress={() => Linking.openURL(this.props.url)} style={styles.icon}>
+          <FontAwesome5 name={'file-download'} size={20} color={'#FFFFFF'} solid />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  body: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between', 
+    alignItems: 'center', 
+    backgroundColor: '#404447',
+    padding: 10,
+  },
+  name: {
+    color: 'white',
+    fontSize: 16
+  },
+  icon: {
+
+  }
+});
