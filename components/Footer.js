a160c786b16eee0ff1d305d206308f1521eca470
Vidal Tomás <vtcheqeeh@gmail.com>
2018-10-11 12:52:45 -0300

    upgrade footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
+
 import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
-export default class AssetExample extends Component {
-  render() {
-    return (
-          <View style={{height: 80, justifySelf: 'flex-end', backgroundColor: 'blue'}}>
-            <Image source={{ uri: 'https://codifiedlondon.blob.core.windows.net/storage/2016/02/reactive-nativingitup.png.800x600_q96.png' }} style={ styles.footerImage }>
-            </Image>
-          </View>
+import {Scene,Router, Actions} from 'react-native-router-flux';
+
+export default class Footer extends Component{
+  render(){
+    return(
+      <View style={styles.body}>
+        <Image source={require('../img/footer.jpg')} style={styles.footerImage} />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  body:{
+    height: 85, 
+    marginTop: 'auto', 
+    backgroundColor: '#48BBEC',
+    borderRadius: 1
+  },
   footerImage:{
     flex: 1,
+    alignSelf: 'center',
+    justifyContent:'center',
     width:'100%',
+    height: '100%',
     paddingTop: 10,
+    marginTop: 'auto',
+    marginBottom:'auto'
   }
 });
