dda6d1b8a34dafacd9be9f90ea7397e119ddf825
Vidal Tomás <vtcheqeeh@gmail.com>
2018-10-25 10:37:18 -0300

    refactory code with store

---
+++ store/index.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux';
+
+import logger from 'redux-logger';
+
+import thunk from 'redux-thunk';
+
+import reducer from '../reducers';
+
+const store = createStore(
+  reducer,
+  {},
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(applyMiddleware(logger, thunk))
+);
+
+export default store;
