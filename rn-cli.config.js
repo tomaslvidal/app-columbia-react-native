89309900ffd9fc1aa450ca97e88cb5af975b5501
Vidal Tomás <vtcheqeeh@gmail.com>
2019-02-14 15:38:44 -0300

    downgrade version

---
+++ rn-cli.config.js
@@ -0,0 +1,6 @@
+// ./rn-cli.config.js
+module.exports = {
+  /// @description Allows you to enable support for JSX files
+  /// The `index.js` file in the root of your project has to be `.js`. 
+  getSourceExts: () => [ 'jsx', 'js' ],
+}
